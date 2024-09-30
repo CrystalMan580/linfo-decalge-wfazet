a8252c3e461a99374da3aae83307d8341eb8a2fe
CrystalMan580 <emnae546@gmail.com>
2024-09-30 19:05:17 +0100

    Create DECALAGE.py

---
+++ DECALAGE.py
@@ -0,0 +1,32 @@
+from numpy import*
+
+ def sasie (n):
+     n =  int(input("donner n :"))
+     while not (3<=n<=15):
+         n = int(input("donner n :"))
+
+ def remplir(n , t):
+     for i in range (0,n-1):
+         t[i] = int(input("doenin"))
+         while not (len<=t[i]<=10 ):
+             t[i] = int(input("doenin"))
+  def alpha(ch):
+     test = True
+     i = 0
+     if ch <= [A , Z]:
+         i =i + 1
+     else:
+         test = False
+     while not():
+         return test
+def decalage(t,n):
+     aux = t[n-1]
+     for i in range(i,n-1):
+         t[i]=t[i-1]
+
+
+
+sasie(n)
+t=array([int]*n)
+remplir(n,t)
+decalage(t,n)
